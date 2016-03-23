ae97fade7110945181f4fb21e715072fb094a22f
Ran Silberman <ran.silberman@gmail.com>
2016-03-23 11:55:26 +0200

    First commit

---
+++ src/main/java/com/playtika/LoggerHelper.java
@@ -0,0 +1,18 @@
+package com.playtika;
+
+import org.apache.log4j.*;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by rans on 21/03/16.
+ */
+public class LoggerHelper {
+    static Logger log = Logger.getLogger(LoggerHelper.class.getName());
+    public static void setStreamingLogLevels() {
+        log.info("Setting log level to [WARN] for streaming example." +
+                " To override add a custom log4j.properties to the classpath.");
+            Logger.getRootLogger().setLevel(Level.WARN);
+//        Logger root = (Logger) LoggerFactory.getLogger();
+//        root.setLevel(Level.INFO);
+    }
+}
